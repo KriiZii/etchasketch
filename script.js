39510557a126d301a48cfb04e1791c7584efa232
KriiZii <39064251+KriiZii@users.noreply.github.com>
2024-08-10 22:03:33 +0200

    boxes now change color on hover and added change size button

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
+const mainContainer = document.querySelector(".mainContainer")
+const buttonContainer = document.querySelector(".buttonContainer")
 const container = document.querySelector(".container")
 
-function createBoxes() {
-    for (let i = 1; i <= 256; i++) {
+const button = document.createElement("button")
+buttonContainer.appendChild(button)
+button.textContent = "Change Size"
+
+function createBoxes(size) {
+    for (let i = 1; i <= (size*size); i++) {
         const box = document.createElement("div")
         box.classList.add("box")
         box.style.backgroundColor = "white"
+        box.style.height = `${800/size}px`
+        box.style.width = `${800/size}px`
         container.appendChild(box)
     }
 }
 
-createBoxes()
+createBoxes(16)
 
 container.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = "orange"
 })
+
+function setGridSize() {
+    let inputSize = prompt("What size would you like the grid to be? (max 100)")
+    if (inputSize > 100) {
+        inputSize = 100
+    }
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild)
+    }
+    createBoxes(inputSize)
+}
+
+button.onclick = () => setGridSize()
